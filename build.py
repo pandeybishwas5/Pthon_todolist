02e4ddba94244918aedcb6e602a5c40a7512cf5e
Bishwas Pandey <pandeybishwas5@gmail.com>
2023-11-10 16:39:01 +1100

    Update build scripts to install dependencies

--- build.py
+++ build.py
@@ -2,12 +2,29 @@
 import subprocess
 import sys
 
+# Install pytest and setuptools
+def install_dependencies():
+    try:
+        subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pytest", "setuptools"])
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing dependencies: {e}")
+        sys.exit(1)
+
 def run_tests():
-    subprocess.run([sys.executable, '-m', 'pytest', 'tests'])
+    try:
+        subprocess.run([sys.executable, '-m', 'pytest', 'tests'])
+    except subprocess.CalledProcessError as e:
+        print(f"Error running tests: {e}")
+        sys.exit(1)
 
 def build_package():
-    subprocess.run([sys.executable, 'setup.py', 'sdist', 'bdist_wheel'])
+    try:
+        subprocess.run([sys.executable, 'setup.py', 'sdist', 'bdist_wheel'])
+    except subprocess.CalledProcessError as e:
+        print(f"Error building package: {e}")
+        sys.exit(1)
 
 if __name__ == '__main__':
+    install_dependencies()
     run_tests()
     build_package()
